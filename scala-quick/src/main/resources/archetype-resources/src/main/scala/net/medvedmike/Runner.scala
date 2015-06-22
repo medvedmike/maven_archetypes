dc726d757c0640357268b40a8fde5038b280bc0a
Ilyin Michael <mr.ilyin.michael@gmail.com>
2015-06-22 21:28:19 +0300

    init

---
+++ scala-quick/src/main/resources/archetype-resources/src/main/scala/net/medvedmike/Runner.scala
@@ -0,0 +1,14 @@
+package net.medvedmike
+
+import org.apache.log4j.Logger
+
+object Runner extends App {
+  private val log = Logger.getLogger(classOf[Runner])
+  log info "Starting ..."
+  //...
+  log info "Shutdown ..."
+}
+
+class Runner {
+
+}
